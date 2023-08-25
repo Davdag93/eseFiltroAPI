9d57618b73ff9f7517a8ca5b2f2ad4d560ba67fe
Davide D'Agostino <davide-d.agostino@outlook.com>
2023-08-25 15:52:33 +0200

    implementata logica searchBar per nome e tipo

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function generaCards(pokemon) {
     const pokedex = document.querySelector("#pokedex");
     pokemon.forEach(pkm => {
         const card = `
-        <div class="col-md-3 mb-4">
+        <div class="col-md-2 mb-4">
         <div class="card">
             <div class="card-image-top p-3"><img src="pokemon.json-master/images/${formatoID(pkm.id)}.png" alt="${pkm.name.english}" title="${pkm.id}" class="img-fluid"></div>
             
@@ -41,3 +41,38 @@ function formatoID(id) {
     }
 }
 
+
+const searchBar = document.querySelector("#cercaPkm")
+// Funzione per filtrare i Pokémon per nome (non case sensitive)
+function filterByPokemonName(name, pokemon) {
+    const lowerCaseName = name.toLowerCase();
+    return pokemon.filter(pkmn => pkmn.name.english.toLowerCase().startsWith(lowerCaseName));
+  }
+  
+  // Funzione per filtrare i Pokémon per tipo (non case sensitive)
+  function filterByPokemonType(type, data) {
+    const lowerCaseType = type.toLowerCase();
+    return data.filter(pkmn => pkmn.type.some(t => t.toLowerCase() === lowerCaseType));
+  }
+
+// Funzione per aggiornare le card dei Pokémon
+function updatePokemonCards(filteredPokemon) {
+  const pokedex = document.querySelector("#pokedex");
+  pokedex.innerHTML = ""; // Svuota il contenitore
+  generaCards(filteredPokemon);
+}
+
+// Aggiungi un ascoltatore all'evento "keyup" sulla searchBar
+searchBar.addEventListener("keyup", (e) => {
+    const inputValue = e.target.value.trim();
+    let pokemonFiltrati = [];
+  
+    if (inputValue.startsWith("type:")) {
+      const typeValue = inputValue.replace("type:", "").trim();
+      pokemonFiltrati = filterByPokemonType(typeValue, pokemon);
+    } else {
+      pokemonFiltrati = filterByPokemonName(inputValue, pokemon);
+    }
+  
+    updatePokemonCards(pokemonFiltrati);
+  });
